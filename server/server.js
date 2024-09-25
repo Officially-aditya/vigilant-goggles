c0bc28657722523c6b48e038c944ef6f0834e560
Officially-aditya <93206009+Officially-aditya@users.noreply.github.com>
2024-09-25 09:11:16 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 4000;
+
+app.get('/route', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'route.json'));
+});
+
+app.use(express.static(path.join(__dirname, '..', 'client')));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
