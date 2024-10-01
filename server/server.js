2e569a76485d8583b2f36ba1b6bcc3768ea0bb60
Officially-aditya <93206009+Officially-aditya@users.noreply.github.com>
2024-10-01 17:44:02 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 4000;
 
 app.get('/route', (req, res) => {
     console.log('Received request for /route');
-    res.sendFile(path.join(__dirname, 'route.json'));
+    res.sendFile(path.join(__dirname, '..', 'route.json'));
 });
-
 
 app.use(express.static(path.join(__dirname, '..', 'client')));
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+module.exports = app;
